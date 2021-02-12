f0b2af6020420c840cfa4b9a63daa720685986bd
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-02-12 10:15:32 +0200

    Add delete

---
+++ Roles/RoleForm/DeleteForm.cs
@@ -0,0 +1,43 @@
+﻿using Roles.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormRoles
+{
+    public partial class DeleteForm : Form
+    {
+        private MyContext _context = new MyContext();
+        public DeleteForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Введіть прізвище користувача,якого потрібно видалити";
+            
+        }       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string surname = textBox1.Text;
+            User d = _context.Users.SingleOrDefault(x => x.Surname==surname);
+            if(d!=null)
+            {
+                _context.Users.Remove(d);
+                _context.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("No");
+            }
+            
+        }
+    }
+}
--- Roles/RoleForm/Form1.cs
+++ Roles/RoleForm/Form1.cs
@@ -37,5 +37,10 @@ namespace RoleForm
         {
             new AddNewUser().ShowDialog();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            new DeleteForm().ShowDialog();
+        }
     }
 }
