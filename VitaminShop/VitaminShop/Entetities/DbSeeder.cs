37bbe44d8e0263db5a2dfb8f9d15032ccfb4540e
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-03-08 18:20:40 +0200

    Add products

--- VitaminShop/VitaminShop/Entetities/DbSeeder.cs
+++ VitaminShop/VitaminShop/Entetities/DbSeeder.cs
@@ -83,26 +83,117 @@ namespace VitaminShop.Entetities
             #endregion
 
             #region Add products
-           // List<string> uniuqe = new List<string>();
+            List<string> uniuqe = new List<string>()
+            {"so25",
+            "sol88",
+            "nf77",
+            "le85",
+            "Dh11",
+            "Hy33"
+            };
 
-            var name_unique = new Faker("en");
+           // var name_unique = new Faker("en");
 
-            var listname = Enumerable.Range(1, 7)
-           .Select(_ =>  name_unique.Lorem.Word())
-           .ToList();
+           // var listname = Enumerable.Range(1, 6)
+           //.Select(_ =>  name_unique.Lorem.Word())
+           //.ToList();
             
 
-            if (context.Products.SingleOrDefault(f => f.UniqueName == listname[0]) == null)
+            if (context.Products.SingleOrDefault(f => f.UniqueName == uniuqe[0]) == null)
                 context.Products.Add(
                         new Product
                         {
-                            UniqueName = listname[0],
-                            Price = ,
-                            Image = "",
-                            Name = ""
+                            UniqueName = uniuqe[0],
+                            Price = 548,
+                            Image = "https://s3.images-iherb.com/sol/sol01736/y/47.jpg",
+                            Name = "Solgar, кожа, ногти и волосы, улучшенная формула с МСМ, 120 таблеток"
                         });
 
+            if (context.Products.SingleOrDefault(f => f.UniqueName == uniuqe[1]) == null)
+                context.Products.Add(
+                        new Product
+                        {
+                            UniqueName = uniuqe[1],
+                            Price = 425,
+                            Image = "https://s3.images-iherb.com/sol/sol35872/y/116.jpg",
+                            Name = "Solgar, витамин D3(холекальциферол), 250 мкг, 120 капсул "
+                        });
 
+            if (context.Products.SingleOrDefault(f => f.UniqueName == uniuqe[2]) == null)
+                context.Products.Add(
+                        new Product
+                        {
+                            UniqueName = uniuqe[2],
+                            Price = 320,
+                            Image = "https://s3.images-iherb.com/now/now00373/y/26.jpg",
+                            Name = "Now Foods, высокоактивный витамин D3, 5000 МЕ, 240 капсул "
+                        });
+
+            if (context.Products.SingleOrDefault(f => f.UniqueName == uniuqe[3]) == null)
+                context.Products.Add(
+                        new Product
+                        {
+                            UniqueName = uniuqe[3],
+                            Price = 1130,
+                            Image = "https://s3.images-iherb.com/lex/lex80148/y/83.jpg",
+                            Name = "Life Extension, Cosmesis Skin Care, подтягивающий и укрепляющий крем для шеи"
+                        });
+            if (context.Products.SingleOrDefault(f => f.UniqueName == uniuqe[4]) == null)
+                context.Products.Add(
+                        new Product
+                        {
+                            UniqueName = uniuqe[4],
+                            Price = 1720,
+                            Image = "https://s3.images-iherb.com/dra/dra00516/y/24.jpg",
+                            Name = "Dragon Herbs, Плацента оленя, 500 мг, 60 капсул"
+                        });
+            if (context.Products.SingleOrDefault(f => f.UniqueName == uniuqe[5]) == null)
+                context.Products.Add(
+                        new Product
+                        {
+                            UniqueName = uniuqe[5],
+                            Price = 520,
+                            Image = "https://s3.images-iherb.com/nap/nap30841/y/4.jpg",
+                            Name = "Nature's, Source of Life, жидкая добавка, со вкусом фруктов, 887,10 мл"
+                        });
+            context.SaveChanges();
+            #endregion
+
+            #region Filters
+            Filter[] filters =
+            {
+                new Filter { FilterNameId =1,FilterValueId=2,ProductId=1},
+                new Filter { FilterNameId =2,FilterValueId=6,ProductId=1},
+                new Filter { FilterNameId =3,FilterValueId=12,ProductId=1},
+
+                new Filter { FilterNameId =1,FilterValueId=2,ProductId=2},
+                new Filter { FilterNameId =2,FilterValueId=7,ProductId=2},
+                new Filter { FilterNameId =3,FilterValueId=12,ProductId=2},
+
+                new Filter { FilterNameId =1,FilterValueId=1,ProductId=3},
+                new Filter { FilterNameId =2,FilterValueId=7,ProductId=3},
+                new Filter { FilterNameId =3,FilterValueId=14,ProductId=3},
+
+                new Filter { FilterNameId =1,FilterValueId=3,ProductId=4},
+                new Filter { FilterNameId =2,FilterValueId=8,ProductId=4},
+
+                new Filter { FilterNameId =1,FilterValueId=5,ProductId=5},
+                new Filter { FilterNameId =2,FilterValueId=7,ProductId=5},
+                new Filter { FilterNameId =3,FilterValueId=12,ProductId=5},
+
+                new Filter { FilterNameId =1,FilterValueId=4,ProductId=6},
+                new Filter { FilterNameId =2,FilterValueId=9,ProductId=6},
+                new Filter { FilterNameId =3,FilterValueId=11,ProductId=6}
+            };
+            foreach (var item in filters)
+            {
+                var f = context.Filters.SingleOrDefault(p => p == item);
+                if (f == null)
+                {
+                    context.Filters.Add(new Filter { FilterNameId = item.FilterNameId, FilterValueId = item.FilterValueId, ProductId = item.ProductId });
+                    context.SaveChanges();
+                }
+            }
             #endregion
         }
 
--- VitaminShop/VitaminShop/Form1.cs
+++ VitaminShop/VitaminShop/Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VitaminShop.Entetities;
+using VitaminShop.Models;
 
 namespace VitaminShop
 {
     public partial class Form1 : Form
     {
+        private readonly MyContext _context;
         public Form1()
         {
             InitializeComponent();
+            _context = new MyContext();
+            //_context = context;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            AutoScroll = true;
+            DbSeeder.SeedDatabase(_context);
+            //var filters = GetListFilters();
+        }
+
+        private List<FilterNameModel> GetListFilters()
+        {
+
+
+            var queryName = from f in _context.FilterNames.AsQueryable()
+                            select f;
+
+            //Зберегла в пропертіз отриманий список назв фільтрів.
+            //filter = queryName;
+
+
+            var queryGroup = from g in _context.FilterNameGroups.AsQueryable()
+                             select g;
+
+            var query = from u in queryName
+                        join g in queryGroup on u.Id equals g.FilterNameId into ua
+                        from aEmp in ua.DefaultIfEmpty()
+                        select new
+                        {
+                            FNameId = u.Id,
+                            FName = u.Name,
+                            FValueId = aEmp != null ? aEmp.FilterValueId : 0,
+                            FValue = aEmp != null ? aEmp.FilterValueOf.Name : null,
+                        };
+
+
+            var groupNames = query.AsEnumerable()
+                      .GroupBy(f => new { Id = f.FNameId, Name = f.FName })
+                      .Select(g => g)
+                      .OrderBy(p => p.Key.Name);
+
+
+
+            var result = from fName in groupNames
+                         select
+                         new FilterNameModel
+                         {
+                             Id = fName.Key.Id,
+                             Name = fName.Key.Name,
+                             Children = fName.Select(x =>
+                                   new FilterValueModel
+                                   {
+                                       Id = x.FValueId,
+                                       Name = x.FValue
+                                   }).OrderBy(l => l.Name).ToList()
+
+                         };
+
+            return result.ToList();
         }
 
     }
